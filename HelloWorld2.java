be49ca5b5fb7fbe1eaf404b2ef1ca2c6f1112d2c
wichitra <674259020@webmail.npru.ac.th>
2024-08-09 11:10:27 +0700

    Add files via upload

---
+++ HelloWorld2.java
@@ -0,0 +1,9 @@
+public class HelloWorld2 {
+    public static void main(string[] args) {
+        system.out.println("hello world");
+        system.out.println("wichitra" + "wilasinee");
+        system.out.println("10 + 4");
+        system.out.println("npru" + 2567);
+        system.out.println(0 + "3" + 2);
+    }
+}
