7a4b87180d4c8b1449b1c6a3e4eca59306f1b2e2
wichitra <674259020@webmail.npru.ac.th>
2024-08-20 12:14:01 +0700

    Add files via upload

---
+++ Tax.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Tax {
+public static void main(String[] args) {
+    Scanner salaryScan = new Scanner (System.in);
+    System.out.print("Enter your salary :");
+    float salaryEnter = salary.nextfloat();
+    float tax = (salaryEnter*10)/100;
+    System.out.println("You tax ="+ tax );
+    salaryScan.close();
+   }
+
+}
